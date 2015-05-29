f495b518c6cc0596193fa16fede381e3a65ea654
Ivan <ivan.herndon@detroitlabs.com>
2015-05-29 16:01:35 -0400

    adding ability to toggle alive status of cell on html

--- main.controller.js
+++ main.controller.js
@@ -6,12 +6,31 @@
 		var vm = this;
 		vm.name = 'Ivan';
 		vm.cellsArray = [];
+		vm.aliveToggle = function(cell){
+			cell.isAlive = !cell.isAlive;
+		}
+		vm.numbersToCalculateSurroundCells = [1,40,39, 41];
 		vm.createCells = function (){
 			for (var i = 0; i < 1600; i ++)
-				vm.cellsArray.push({position: i});
+				vm.cellsArray.push(new Cell(i, false, false));
+		}
+		vm.getCellsAroundCheckedCell = function(cell){
+			var array=[];
+			for (var i = 0; i < vm.numbersToCalculateSurroundCells; i++){
+
+				if(Number(cell.position) + vm.numbersToCalculateSurroundCells[i] !== undefined){
+					array.push(Number(cell.position) + vm.numbersToCalculateSurroundCells[i]);
+				}
+				if(Number(cell.position) - vm.numbersToCalculateSurroundCells[i] !== undefined){
+					array.push(Number(cell.position) - vm.numbersToCalculateSurroundCells[i]);
+				}
+			}
+			return array;
 		}
 		vm.createCells();
-		function cell (position, isAlive, nextIteration){
+
+
+		function Cell (position, isAlive, nextIteration){
 			this.position = position;
 			this.isAlive = isAlive;
 			this.nextIteration = nextIteration;
@@ -20,9 +39,9 @@
 			}
 			this.checkStatus = function(){
 				if(this.isAlive){
-					return {background: red}
+					return {'background': 'red'}
 				} else {
-					return {background: none}
+					return {'background': 'none'}
 				}
 			}
 		}
