14e6d09909f2032274ed65377721d75d5af48d2b
Ivan <ivan.herndon@detroitlabs.com>
2015-05-29 17:06:05 -0400

    working on algorithm that goes through the game

--- main.controller.js
+++ main.controller.js
@@ -9,23 +9,63 @@
 		vm.aliveToggle = function(cell){
 			cell.isAlive = !cell.isAlive;
 		}
-		vm.numbersToCalculateSurroundCells = [1,40,39, 41];
+		vm.numbersToCalculateSurroundingCells = [1,40,39, 41];
 		vm.createCells = function (){
 			for (var i = 0; i < 1600; i ++)
 				vm.cellsArray.push(new Cell(i, false, false));
 		}
 		vm.getCellsAroundCheckedCell = function(cell){
 			var array=[];
-			for (var i = 0; i < vm.numbersToCalculateSurroundCells; i++){
-
-				if(Number(cell.position) + vm.numbersToCalculateSurroundCells[i] !== undefined){
-					array.push(Number(cell.position) + vm.numbersToCalculateSurroundCells[i]);
+			
+			for (var i = 0; i < vm.numbersToCalculateSurroundingCells.length; i++){
+				var added = cell.position + vm.numbersToCalculateSurroundingCells[i];
+				var subtracted = cell.position - vm.numbersToCalculateSurroundingCells[i];
+				if(added >= 0 && added < 1600 ){
+					array.push(vm.cellsArray[added]);	
 				}
-				if(Number(cell.position) - vm.numbersToCalculateSurroundCells[i] !== undefined){
-					array.push(Number(cell.position) - vm.numbersToCalculateSurroundCells[i]);
+				if(subtracted >= 0 && subtracted < 1600 ){
+					array.push(vm.cellsArray[subtracted]);
 				}
 			}
 			return array;
+		}
+		vm.iterator = function(){
+			vm.cellsArray.forEach(function(cell){
+				var alive = 0;
+				var tempArray = vm.getCellsAroundCheckedCell(cell);
+				tempArray.forEach(function(item){
+					if (item.isAlive === true)
+						alive ++;
+				});
+				if(cell.isAlive === true && (alive<2 || alive > 3)) {
+					cell.nextIteration = false;
+				} else if (cell.isAlive === false && alive === 3) {
+					cell.nextIteration = true;
+				} else if (cell.isAlive === false){
+					cell.nextIteration = false;
+				} else {
+					cell.nextIteration = true;
+				}
+			})
+		}
+		vm.changeStatusAll = function(){
+			vm.cellsArray.forEach(function(cell){
+				cell.changeStatus();
+			})
+		}
+		vm.runGameOnInterval = function(repititions){
+			console.log('started');
+			var x = 0;
+			var intervals = $interval (function(){
+				vm.iterator();
+				vm.changeStatusAll();
+				x++;
+
+				if (x === repititions){
+					$interval.cancel(intervals)
+					console.log('stopped');
+				}
+			}, 150)
 		}
 		vm.createCells();
 
